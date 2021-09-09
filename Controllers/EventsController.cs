96fda6cda3824a6f595cabc9da932faea5bce3b5
Tiah Keefe <tetered12@gmail.com>
2021-09-08 19:50:58 -0500

    Add project files.

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents1.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name, string description)
+        {
+            Events.Add(name, description);
+            return Redirect("/Events");
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodingEvents1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
